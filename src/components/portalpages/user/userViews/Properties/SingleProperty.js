77def27bdfbc1a36d777b7a668690a2e93154cad
Katie Hansen <khansen0415@gmail.com>
2019-12-27 09:05:51 -0500

    build error boundary and add to single prop page

--- src/components/portalpages/user/userViews/Properties/SingleProperty.js
+++ src/components/portalpages/user/userViews/Properties/SingleProperty.js
@@ -5,6 +5,7 @@ import Messages from '../../../../utilities/PropertyComponents/Messages'
 import Maintenance from '../../../../utilities/PropertyComponents/Maintenance'
 import Cost from '../../../../utilities/PropertyComponents/Cost'
 import PropertyInfo from '../../../../utilities/PropertyComponents/PropertyInfo'
+import ErrorBoundary from '../../../../utilities/ErrorBoundary'
 
 class SingleProperty extends Component {
     static contextType = BrokerContext
@@ -18,23 +19,33 @@ class SingleProperty extends Component {
     render(){
         return (
             <div className="single-property-container">
-                <PropertyInfo 
-                    id={this.props.id}
-                    type='user'
-                />
-                <Features 
-                    id={this.props.id}
-                />
-                <Messages 
-                    id={this.props.id}
-                />
-                <Maintenance
-                    id={this.props.id}
-                />
-               <Cost
-                    id={this.props.id}
-                    setCosts={this.setCosts}
-                />
+                <ErrorBoundary>
+                    <PropertyInfo 
+                        id={this.props.id}
+                        type='user'
+                    />
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <Features 
+                        id={this.props.id}
+                    />
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <Messages 
+                        id={this.props.id}
+                    />
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <Maintenance
+                        id={this.props.id}
+                    />
+                </ErrorBoundary>
+                <ErrorBoundary>
+                <Cost
+                        id={this.props.id}
+                        setCosts={this.setCosts}
+                    />
+                </ErrorBoundary>
             </div>
         )
     }
---
+++ src/components/utilities/ErrorBoundary.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+
+class ErrorBoundary extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            error: null
+        }
+    }
+
+    static getDerivedStateFromError(error){
+        return { error: true }
+    }
+
+    componentDidCatch(error, errorInfo){
+        console.log(error)
+        console.log(errorInfo)
+    }
+
+
+    render(){
+        if(this.state.error) {
+            return (
+                <p>Something went wrong</p>
+                )
+        }
+        return this.props.children
+    }
+}
+
+export default ErrorBoundary
