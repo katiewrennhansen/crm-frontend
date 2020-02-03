401ce30c36704d6790f8305e5958b75ab35269b9
Katie Hansen <khansen0415@gmail.com>
2020-02-03 10:44:49 -0700

    try out button text on user properties

--- src/components/portalpages/user/userViews/Properties/Properties.js
+++ src/components/portalpages/user/userViews/Properties/Properties.js
@@ -43,6 +43,7 @@ class Properties extends Component {
                             className="action-icon" 
                             aria-label="add property" 
                         />
+                        <p>Add Property</p>
                     </Link>
                 </div>
                 <div className='broker-properties-grid'>
